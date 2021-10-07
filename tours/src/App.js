a0081788d5dd0b28a251c724e85d0dea2a143e0b
abhishek-p-au16 <abhishekp9991999@gmail.com>
2021-10-07 19:10:22 +0530

    fetched data and listeed

--- tours/src/App.js
+++ tours/src/App.js
@@ -5,7 +5,41 @@ import Tours from './Tours'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading,setLoading] =useState(true)
+  const [tours,setTour] = useState([]);
+  const fetchTours = async()=>{
+    setLoading(true);
+    try{
+      const response = await fetch(url);
+    const tours = await response.json();
+    setLoading(false);
+    setTour(tours);  
+    console.log(tours);
+
+    }
+    catch(err){
+      console.log(err);
+
+    }
+  }
+
+  useEffect(()=>{
+    fetchTours();
+  },[]);
+
+
+  if(loading){
+    return(
+      <main>
+        <Loading/>
+      </main>
+    )
+  }
+  return (
+    <main>
+      <Tours tours={tours}/>
+    </main>
+  )
 }
 
 export default App
