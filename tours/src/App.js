3ee8f392547c13cb2ec48edd474059814127bb67
abhishek-p-au16 <abhishekp9991999@gmail.com>
2021-10-07 20:11:45 +0530

    remove button added

--- tours/src/App.js
+++ tours/src/App.js
@@ -7,6 +7,10 @@ const url = 'https://course-api.com/react-tours-project'
 function App() {
   const [loading,setLoading] =useState(true)
   const [tours,setTour] = useState([]);
+
+  const removeTour = (id)=>{
+    const newTours = tours.filter((tour)=>tour.id !==id);
+    setTour(newTours)}
   const fetchTours = async()=>{
     setLoading(true);
     try{
@@ -37,7 +41,7 @@ function App() {
   }
   return (
     <main>
-      <Tours tours={tours}/>
+      <Tours tours={tours} removeTour={removeTour}/>
     </main>
   )
 }
